8a2ee3fb07e80f8163c6fb00d53168fa3b66b28e
Gervy Salinas <play_main_1001@outlook.com>
2022-07-01 20:22:57 -0500

    routing active

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+
+
+const routes: Routes = [
+  {path: '', component:LoginComponent},
+  {path: 'main', component:MainComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
