524a7e11a5acfc1017de42777b035de1ec1fcb50
Gervy Salinas <play_main_1001@outlook.com>
2022-07-03 23:51:30 -0500

    scss ang material

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -23,7 +23,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent{ 
 
